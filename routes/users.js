62f77f7062ead4c0923e5bc552a0efb9588a4cfd
Ben Perlmutter <benperlmutter94@gmail.com>
2020-05-22 12:58:23 -0500

    added update route for password and delete for user and all associated data

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,68 @@ const validateCreateUser = [
     handleValidationErrors
 ]
 
+
+//sign up
+router.post(
+  "/",
+  validateCreateUser,
+  asyncHandler(async (req, res) => {
+    console.log(req.body);
+    const { email, password, username, firstName, lastName } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(
+      { email, password, username, firstName, lastName },
+      hashedPassword
+    );
+    const user = await db.User.create({
+      email,
+      hashedPassword,
+      username,
+      firstName,
+      lastName,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    console.log("USER POSTED");
+    const token = getUserToken(user);
+
+    res.status(201).json({ user: { id: user.id }, token });
+  })
+);
+
+//log in
+router.post(
+  "/token",
+  asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const user = await db.User.findOne({ where: { email: email } });
+      const isPassword = await bcrypt.compare(
+        password,
+        user.hashedPassword.toString()
+      );
+      console.log(user);
+      if (!isPassword) {
+        const err = new Error("Login failed");
+        err.status = 401;
+        err.title = "Login failed";
+        err.errors = ["The provided credentials were invalid."];
+        return next(err);
+      }
+      const token = getUserToken(user);
+      res.json({ token, user: { id: user.id } });
+    } catch (e) {
+      const err = new Error("Login failed");
+      err.status = 401;
+      err.title = "Login failed";
+      err.errors = ["The provided credentials were invalid."];
+      return next(err);
+    }
+  })
+);
+
+//get user profile page
 router.get("/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
     const userId = parseInt(req.params.id, 10);
     try{
@@ -54,6 +116,8 @@ router.get("/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
 
 }));
 
+
+//update user profile info
 router.put("/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
     const userId = parseInt(req.params.id, 10);
     try{
@@ -68,6 +132,8 @@ router.put("/:id(\\d+)", requireAuth, asyncHandler(async (req, res) => {
     }
 }));
 
+
+//change password
 router.put("/:id(\\d+)/password", requireAuth, asyncHandler(async (req, res) => {
     const userId = parseInt(req.params.id, 10);
     try{
@@ -77,8 +143,16 @@ router.put("/:id(\\d+)/password", requireAuth, asyncHandler(async (req, res) => 
         if(isPassword){
             const hashedPassword = await bcrypt.hash(newPassword, 10);
             await user.update({hashedPassword});
+            const success = true
+            res.json({user, message: "Your password has been successfully updated.", success});
+        } else{
+            const success = false;
+            res.json({
+              user,
+              message: "There was a problem updating your password. Please try again.",
+              success,
+            });
         }
-        res.json({user, message: "Your password has been successfully updated."});
     } catch(err){
         console.error(err);
         res.message = err;
@@ -86,54 +160,52 @@ router.put("/:id(\\d+)/password", requireAuth, asyncHandler(async (req, res) => 
     }
 }));
 
-router.post('/', validateCreateUser, asyncHandler ( async (req, res) => {
-    console.log(req.body);
-    const {email, password, username, firstName, lastName} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log( {email, password, username, firstName, lastName}, hashedPassword)
-        const user = await db.User.create({
-          email,
-          hashedPassword,
-          username,
-          firstName,
-          lastName,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        });
-
-    console.log("USER POSTED");
-    const token = getUserToken(user);
-
-    res.status(201).json({user: {id: user.id}, token });
+//delete account
+router.delete("/:id(\\d+)/delete", requireAuth, asyncHandler(async (req, res) => {
+    const userId = parseInt(req.params.id, 10);
+    try{
+        
+        const { deletePassword, confirmDeletePassword } = req.body;
+        if(deletePassword !== confirmDeletePassword){
+            const success = false;
+            res.json({
+              user,
+              message: "It looks like yor passwords didn't match. Please try again",
+              success,
+            });
+        }
+        const user = await db.User.findByPk(userId);
+        const isPassword = await bcrypt.compare(deletePassword, user.hashedPassword.toString());
+        if(isPassword){
+            await db.Checkin.destroy({where: {
+                userId: userId
+            }})
+            await db.List.destroy({where: {
+                userId: userId
+            }})
+            await db.LikedBrewery.destroy({
+              where: {
+                userId: userId,
+              },
+            });
+            await user.destroy();
+            const success = true;
+            res.json({user, message: "The user and all associated data has successfully been deleted.", success});
+        } else{
+            const success = false;
+            res.json({
+              user,
+              message: "It seems you didn't enter the correct password. Please try again",
+              success,
+            });
+        }
+    } catch(err){
+        console.error(err);
+        res.message = err;
+        res.status(500).send();
+    }
 }));
 
-router.post("/token", asyncHandler(async (req, res, next)=>{
-    const {email, password} = req.body;
-    try{
-        const user = await db.User.findOne({ where: { email: email } });
-        const isPassword = await bcrypt.compare(
-          password,
-          user.hashedPassword.toString()
-        );
-        console.log(user);
-        if (!isPassword) {
-          const err = new Error("Login failed");
-          err.status = 401;
-          err.title = "Login failed";
-          err.errors = ["The provided credentials were invalid."];
-          return next(err);
-        }
-        const token = getUserToken(user);
-        res.json({ token, user: { id: user.id } });
-    }catch(e){
-        const err = new Error("Login failed");
-        err.status = 401;
-        err.title = "Login failed";
-        err.errors = ["The provided credentials were invalid."];
-        return next(err);
-    }
-    
-}))
 
 
 module.exports = router;
